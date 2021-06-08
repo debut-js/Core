dd79f26338627562955d36c74115251621c31da6
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2021-06-08 12:56:58 +0300

    add transport exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+export { TinkoffTransport } from './transports/tinkoff';
+export { BinanceTransport } from './transports/binance';
 export * from './cli/tester/history';
 export { Debut } from './modules/debut';
