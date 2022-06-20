4262b60d37aeeac3eede5900b123c79b5a5840cb
Dmitriy Yurov <dyurov@tradingview.com>
2022-06-20 11:07:10 +0300

    finder with cluster

--- src/cli/finder-cli.ts
+++ src/cli/finder-cli.ts
@@ -1,7 +1,9 @@
 import { cli, file } from '@debut/plugin-utils';
 import { GeneticType, GeneticWFOType, GenticWrapperOptions } from '@debut/types';
+import cluster from 'cluster';
 import { DebutError, ErrorEnvironment } from '../modules/error';
 import { GeneticWrapper } from './tester/genetic';
+import { GeneticWorker } from './tester/genetic-worker';
 
 type Params = {
     bot: string;
@@ -38,67 +40,67 @@ const {
 let schema: cli.BotData | null;
 
 (async function () {
-    try {
-        schema = await cli.getBotData(bot);
-    } catch (e) {
-        throw new DebutError(ErrorEnvironment.Tester, `${e}`);
-    }
+    if (cluster.isPrimary) {
+        try {
+            schema = await cli.getBotData(bot);
+        } catch (e) {
+            throw new DebutError(ErrorEnvironment.Tester, `${e}`);
+        }
 
-    if (!schema) {
-        process.stdout.write('Genetic CLI error: Incorrect configuration');
-        return;
-    }
+        if (!schema) {
+            process.stdout.write('Genetic CLI error: Incorrect configuration');
+            return;
+        }
 
-    const options: GenticWrapperOptions = {
-        score: schema.meta.score,
-        stats: schema.meta.stats,
-        ticksFilter: schema.meta.ticksFilter,
-        validateSchema: schema.meta.validate,
-        validateForwardStats: schema.meta.validateStats,
-        create: schema.meta.create,
-        days,
-        generations: gen,
-        populationSize: pop,
-        log,
-        ohlc,
-        gapDays: gap,
-        wfo,
-        gaType,
-        gaContinent,
-    };
-    const { configs, meta } = schema;
-    const cfgKeys = Object.keys(configs);
-    const originCfg = configs[ticker || cfgKeys[0]];
+        const options: GenticWrapperOptions = {
+            ticksFilter: schema.meta.ticksFilter,
+            validateSchema: schema.meta.validate,
+            validateForwardStats: schema.meta.validateStats,
+            days,
+            generations: gen,
+            populationSize: pop,
+            log,
+            gapDays: gap,
+            wfo,
+            gaType,
+            gaContinent,
+        };
+        const { configs, meta } = schema;
+        const cfgKeys = Object.keys(configs);
+        const originCfg = configs[ticker || cfgKeys[0]];
 
-    if (meta.validate) {
-        options.validateSchema = meta.validate;
-    }
+        if (meta.validate) {
+            options.validateSchema = meta.validate;
+        }
 
-    const stockPath = crypt ? 'crypt.json' : 'stocks.json';
-    const stocks = JSON.parse(file.readFile(stockPath));
-    const currentTicker = stocks.pop();
+        const stockPath = crypt ? 'crypt.json' : 'stocks.json';
+        const stocks = JSON.parse(file.readFile(stockPath));
+        const currentTicker = stocks.pop();
 
-    if (!currentTicker) {
-        return;
-    }
+        if (!currentTicker) {
+            return;
+        }
 
-    const genetic = new GeneticWrapper(options);
-    const config = { ...originCfg, ticker: currentTicker, amount: Number(amount) };
+        const genetic = new GeneticWrapper(options);
+        const config = { ...originCfg, ticker: currentTicker, amount: Number(amount) };
 
-    file.saveFile(stockPath, stocks);
+        file.saveFile(stockPath, stocks);
 
-    let stats = await genetic.start(meta.parameters, config);
-    const path = `public/reports/${bot}/${config.interval}/tickers/${currentTicker}.json`;
+        let stats = await genetic.start(meta.parameters, config);
+        const path = `public/reports/${bot}/${config.interval}/tickers/${currentTicker}.json`;
 
-    stats = stats
-        .map((item, index) => {
-            item.config.id = index;
-            return item;
-        })
-        .filter((stat) => stat.stats);
+        stats = stats
+            .map((item, index) => {
+                item.config.id = index;
+                return item;
+            })
+            .filter((stat) => stat.stats);
 
-    if (stats.length) {
-        file.ensureFile(path);
-        file.saveFile(path, stats);
+        if (stats.length) {
+            file.ensureFile(path);
+            file.saveFile(path, stats);
+        }
+    } else {
+        new GeneticWorker(ohlc, ticker, bot);
     }
 })();
